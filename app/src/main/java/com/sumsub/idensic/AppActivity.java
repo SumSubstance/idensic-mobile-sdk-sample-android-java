5594aaa7178a6ebe9d604d852a915cf9636eaa26
Данил Терновых <danil.t@sumsub.com>
2020-11-03 12:46:57 +0300

    update to version 1.13.4

--- app/src/main/java/com/sumsub/idensic/AppActivity.java
+++ app/src/main/java/com/sumsub/idensic/AppActivity.java
@@ -1,14 +1,31 @@
 package com.sumsub.idensic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import timber.log.Timber;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class AppActivity extends AppCompatActivity {
+import com.google.android.gms.security.ProviderInstaller;
+
+public class AppActivity extends AppCompatActivity implements ProviderInstaller.ProviderInstallListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
+
+        ProviderInstaller.installIfNeededAsync(this, this);
+    }
+
+    @Override
+    public void onProviderInstalled() {
+        Timber.d("onProviderInstalled");
+    }
+
+    @Override
+    public void onProviderInstallFailed(@NonNull int errorCode, @NonNull Intent recoveryIntent) {
+        Timber.d("onProviderInstallFailed: errorCode=%s", errorCode);
     }
 }
